0a4dbc6b7d409bd532446c75d46fc04bc193f4a8
mcgrathgideo <cmgrath@gideo.video>
2024-11-05 23:51:46 -0500

    title now added to playDescription

--- src/components/PlayDescription.jsx
+++ src/components/PlayDescription.jsx
@@ -1,7 +1,8 @@
 // src/components/PlayDescription.jsx
-export default function PlayDescription({ description }) {
+export default function PlayDescription({ title, description }) {
   return (
     <div className="bg-[#2a2a2a] p-4 md:p-8 rounded-lg max-w-4xl mx-auto px-4" >
+      <h3 className="text-lg md:text-xl font-semibold text-white">{title}: </h3>
       <p className="text-gray-300 text-sm md:text-base">{description}</p>
     </div>
   )
